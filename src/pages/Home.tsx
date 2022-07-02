5a6f21afc867ff1edb82ec3601e61e5a187644e0
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-07-02 15:26:27 -0300

    feat: implementing functions handleAddTask, handleToggleTaskDone and handleRemoveTask

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,30 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const data = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }
+    setTasks(state => [...state, data]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const taskExists = tasks.find(task => task.id === id);
+
+    if (taskExists) {
+      const tasksUpdated = tasks.map((task) => {
+        if (task.id === id) task.done = !task.done;
+        return task;
+      });
+
+      setTasks(tasksUpdated);
+    }
+
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(state => state.filter(task => task.id !== id));
   }
 
   return (
@@ -26,10 +41,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
